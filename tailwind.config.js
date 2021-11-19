a496d067fa743307e8a4d978284640f59294a1e4
Michele <michele.rella10@hotmail.com>
2021-11-19 18:43:38 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        box: '#efefef',
+        purple: '#af067d',
+      },
+    },
+    
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
